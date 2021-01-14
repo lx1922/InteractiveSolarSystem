9447ad48cc1109ccbe9c731ad945177353360c0b
Rishith Nellippillil <rishu.ks@gmail.com>
2021-01-14 00:06:40 -0500

    Made and added sun, mercury, venus, earth and mars.

--- Java/SolarSystem/src/solarSystem/SolarApplication.java
+++ Java/SolarSystem/src/solarSystem/SolarApplication.java
@@ -1,44 +1,70 @@
 package solarSystem;
 
-import javafx.animation.Animation;
-import javafx.animation.Interpolator;
-import javafx.animation.RotateTransition;
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.Background;
-import javafx.scene.layout.BackgroundFill;
-import javafx.scene.layout.CornerRadii;
-import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.PhongMaterial;
 import javafx.scene.shape.Sphere;
-import javafx.scene.transform.Rotate;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 public class SolarApplication extends Application {
 	
-//    private Image getImageFromResource(String resourcePath) {
-//        return new Image(this.getClass().getResourceAsStream(resourcePath));
-//    }
+    private Image getImageFromResource(String resourcePath) {
+        return new Image(this.getClass().getResourceAsStream(resourcePath));
+    }
 
 	public void start(Stage stage) throws Exception {
-		Sphere sun = new Sphere(50);
+		Sphere sun = new Sphere(75);
 		sun.setTranslateX(600);
 		sun.setTranslateY(450);
+		PhongMaterial material_sun = new PhongMaterial();
+        material_sun.setDiffuseMap(getImageFromResource("sunmap.jpg"));
+        material_sun.setSpecularColor(Color.WHITE);
+        material_sun.setSpecularPower(0.1);
+        sun.setMaterial(material_sun);
 		
-		Group Sun = new Group();
-		Sun.getChildren().add(sun);
+		Sphere mercury = new Sphere(5);
+		mercury.setTranslateX(725);
+		mercury.setTranslateY(450);
+		PhongMaterial material_mercury = new PhongMaterial();
+        material_mercury.setDiffuseMap(getImageFromResource("mercurymap.jpg"));
+        mercury.setMaterial(material_mercury);
+		
+		Sphere venus = new Sphere(9);
+		venus.setTranslateX(775);
+		venus.setTranslateY(450);
+		PhongMaterial material_venus = new PhongMaterial();
+        material_venus.setDiffuseMap(getImageFromResource("venusmap.jpg"));
+        venus.setMaterial(material_venus);
+		
+		Sphere earth = new Sphere(12);
+		earth.setTranslateX(825);
+		earth.setTranslateY(450);
+		PhongMaterial material_earth = new PhongMaterial();
+        material_earth.setDiffuseMap(getImageFromResource("earthmap1k.jpg"));
+        earth.setMaterial(material_earth);
+		
+		Sphere mars = new Sphere(14);
+		mars.setTranslateX(900);
+		mars.setTranslateY(450);
+		PhongMaterial material_mars = new PhongMaterial();
+        material_mars.setDiffuseMap(getImageFromResource("mars_1k_color.jpg"));
+        mars.setMaterial(material_mars);
+		
+		Group planets = new Group();
+		planets.getChildren().add(sun);
+		planets.getChildren().add(mercury);
+		planets.getChildren().add(venus);
+		planets.getChildren().add(earth);
+		planets.getChildren().add(mars);
 		
 		PerspectiveCamera camera = new PerspectiveCamera();
 		
-		Scene scene = new Scene(Sun, 1200, 900);
+		Scene scene = new Scene(planets, 1200, 900);
 		scene.setFill(Color.BLACK); 
-		
 		
 		scene.setCamera(camera);
 		stage.setTitle("Solar System");
