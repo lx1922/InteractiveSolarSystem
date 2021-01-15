948893f7b884b7dda4cc64db9a6acd85b68bda7f
root <root@LX1922.localdomain>
2021-01-14 20:45:45 -0500

    Added earth and mercury

--- Java/SolarSystem/src/solarSystem/SolarApplication.java
+++ Java/SolarSystem/src/solarSystem/SolarApplication.java
@@ -8,10 +8,12 @@ import javafx.animation.PathTransition;
 import javafx.animation.RotateTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.beans.binding.Bindings;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -31,9 +33,14 @@ import javafx.util.Duration;
 
 public class SolarApplication extends Application {
 	private static PerspectiveCamera camera;
+	
+	private Image getImageFromResource(String resourcePath) {
+		return new Image(this.getClass().getResourceAsStream(resourcePath));
+	}
 
 	public void start(Stage stage) throws Exception {
 		stage.setTitle("Solar System");
+		
 		
 		// makes the sun
 		Sphere sun = new Sphere(75);
@@ -41,35 +48,38 @@ public class SolarApplication extends Application {
 		// makes the mercury
 		Sphere mercury = new Sphere(5);
 		
+		Sphere earth = new Sphere(5);
+		
 		// center the sun
 		sun.setTranslateX(600);
 		sun.setTranslateY(450);
-		
+
+		PhongMaterial material = new PhongMaterial();
+		material.setDiffuseMap(getImageFromResource("sunmap.jpg"));
+		material.setSpecularPower(0);
+		sun.setMaterial(material);
+	    
+	    sun.setMaterial(material);
+        
 		Group Sun = new Group();
 		Sun.getChildren().add(sun);
 		Sun.getChildren().add(mercury);
+		Sun.getChildren().add(earth);
 		
         Ellipse ellipseMercury = getMercuryEllipse(sun);
         
         mercuryTransition(ellipseMercury, mercury);
         
+        Ellipse ellipseEarth = getEarthEllipse(sun);
+        
+        earthTransition(ellipseEarth, earth);
+        
+		Sun.getChildren().add(ellipseEarth);
 		Sun.getChildren().add(ellipseMercury);
-		
-		camera = new PerspectiveCamera( true );
-        camera.setTranslateX( 600 );
-        camera.setTranslateY( 450 );
-        camera.setTranslateZ( 0 );
-        camera.setFieldOfView(90.0);
-        camera.setFarClip(1500.0);
-        camera.setNearClip(0.01);
-		
 
 		Scene scene = new Scene(Sun, 1200, 900);
-		
-		scene.setCamera( camera );
-        scene.setOnMouseMoved( new MouseLook() );
         
-		scene.setFill(Color.TRANSPARENT);
+		scene.setFill(Color.BLACK);
 		
 		stage.setScene(scene);
 		stage.show();
@@ -80,13 +90,24 @@ public class SolarApplication extends Application {
 		Ellipse ellipseMercury = new Ellipse();
         ellipseMercury.setCenterX(sun.getTranslateX());
         ellipseMercury.setCenterY(sun.getTranslateY());
-
-        ellipseMercury.setRadiusX(sun.getBoundsInLocal().getWidth() / 2.0 + 1.01671388 * 170);
-        ellipseMercury.setRadiusY(sun.getBoundsInLocal().getHeight() / 2.0 + 0.98329134 * 170);
+        ellipseMercury.setRadiusX(sun.getBoundsInLocal().getWidth() / 2.0 + 0.466697  * 170);
+        ellipseMercury.setRadiusY(sun.getBoundsInLocal().getHeight() / 2.0 + 0.307499 * 170);
         ellipseMercury.setStroke(Color.WHITE);
         ellipseMercury.setStrokeWidth(1);
         ellipseMercury.setFill(Color.TRANSPARENT);
         return ellipseMercury;
+	}
+	
+	public Ellipse getEarthEllipse(Sphere sun) {
+		Ellipse ellipseEarth = new Ellipse();
+        ellipseEarth.setCenterX(sun.getTranslateX());
+        ellipseEarth.setCenterY(sun.getTranslateY());
+        ellipseEarth.setRadiusX(sun.getBoundsInLocal().getWidth() / 2.0 + 1.01671388  * 170);
+        ellipseEarth.setRadiusY(sun.getBoundsInLocal().getHeight() / 2.0 + 0.98329134 * 170);
+        ellipseEarth.setStroke(Color.WHITE);
+        ellipseEarth.setStrokeWidth(1);
+        ellipseEarth.setFill(Color.TRANSPARENT);
+        return ellipseEarth;
 	}
 	
 	public void mercuryTransition(Ellipse ellipseMercury, Sphere mercury) {
@@ -94,96 +115,24 @@ public class SolarApplication extends Application {
         transitionEarth.setPath(ellipseMercury);
         transitionEarth.setNode(mercury);
         transitionEarth.setInterpolator(Interpolator.LINEAR);
+        transitionEarth.setDuration(Duration.seconds(2.40846));
+        transitionEarth.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        transitionEarth.setCycleCount(Timeline.INDEFINITE);
+        transitionEarth.play();
+	}
+	
+	public void earthTransition(Ellipse ellipseEarth, Sphere earth) {
+		PathTransition transitionEarth = new PathTransition();
+        transitionEarth.setPath(ellipseEarth);
+        transitionEarth.setNode(earth);
+        transitionEarth.setInterpolator(Interpolator.LINEAR);
         transitionEarth.setDuration(Duration.seconds(10.000017421));
         transitionEarth.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
         transitionEarth.setCycleCount(Timeline.INDEFINITE);
         transitionEarth.play();
 	}
 	
-	static class MouseLook implements EventHandler <MouseEvent> {
-	    private static Rotate rotation;
-	    private static int oldX, newX;
-	    private static boolean alreadyMoved = false;
-
-	    @Override
-	    public void handle(MouseEvent event) {
-	        if ( alreadyMoved ) {
-	            newX = (int) event.getScreenX();
-
-	            if ( oldX < newX ) { // if mouse moved to right
-	                rotation = new Rotate( 10.0,
-	                        // camera rotates around its location
-	                        camera.getTranslateX(), camera.getTranslateY(), camera.getTranslateZ(),
-	                        Rotate.Y_AXIS );
-
-
-	            } else if ( oldX > newX ) { // if mouse moved to left
-	                rotation = new Rotate( -10.0,
-	                        // camera rotates around its location
-	                        camera.getTranslateX(), camera.getTranslateY(), camera.getTranslateZ(),
-	                        Rotate.Y_AXIS );
-
-	            }
-	            camera.getTransforms().addAll( rotation );
-
-	            oldX = newX;
-	        } else {
-	            oldX = (int) event.getScreenX();
-	            alreadyMoved = true;
-	        }
-	    }
-	}
-	
-	
-	
 	public static void main(String[] args) {
 		launch(args);
 	}
 }
-
-
-
-
-
-
-//private Image getImageFromResource(String resourcePath) {
-//return new Image(this.getClass().getResourceAsStream(resourcePath));
-//}
-
-
-
-
-
-
-
-
-
-
-
-
-
-//HBox hBox = new HBox();
-//Scene scene = new Scene(hBox, 1200, 900);
-//hBox.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-//Sphere sphere = new Sphere(200);
-//PhongMaterial material = new PhongMaterial();
-////material.setDiffuseMap(getImageFromResource("sunmap.jpg"));
-////material.setSpecularPower(1);
-////sphere.setMaterial(material);
-//hBox.getChildren().add(sphere);
-//sphere.setTranslateX(400);
-//sphere.setTranslateY(250);
-//RotateTransition rotateTransition = new RotateTransition();
-//rotateTransition.setNode(sphere);
-//rotateTransition.setDuration(Duration.seconds(15));
-//rotateTransition.setAxis(Rotate.Y_AXIS);
-//rotateTransition.setFromAngle(0);
-//rotateTransition.setToAngle(360);
-//rotateTransition.setInterpolator(Interpolator.LINEAR);
-//rotateTransition.setCycleCount(Animation.INDEFINITE);
-//rotateTransition.play();
-////PerspectiveCamera camera = new PerspectiveCamera();
-////camera.setTranslateY(-100);
-////scene.setCamera(camera);
-//stage.setScene(scene);
-//stage.show();
